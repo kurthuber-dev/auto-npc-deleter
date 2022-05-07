803a170b22a8168fd9406e9749e742d5f7d64476
kurthuber-dev <75269638+kurthuber-dev@users.noreply.github.com>
2022-05-07 17:46:34 +0200

    Create fxmanifest.lua

---
+++ fxmanifest.lua
@@ -0,0 +1,19 @@
+fx_version 'adamant'
+
+game 'gta5'
+
+author 'Flolo'
+description 'Remove NPCS from an Area'
+version '1.0'
+
+shared_scripts {
+	'config.lua'
+}
+
+client_scripts {
+    'client.lua'
+}
+
+server_scripts {
+    'server.lua'
+}
